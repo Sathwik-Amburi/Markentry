97e5518206aa92347cf0701017c6915263dd04b7
Sathwik Amburi <sathwik.amburi@gmail.com>
2025-01-10 14:58:18 +0100

    fix merge issue

--- markentry/main.py
+++ markentry/main.py
@@ -26,25 +26,6 @@ def main():
 		),
 	]
 
-<<<<<<< HEAD
-for idx, user_input in enumerate(inputs):
-    print()
-    print(f"--- Conversation Turn {idx + 1} ---")
-    print()
-    print(f"User: {user_input}")
-    print()
-    for update in graph.stream(
-        user_input,
-        config=thread_config,
-        stream_mode="updates",
-    ):
-        for node_id, value in update.items():
-            if isinstance(value, dict) and value.get("messages", []):
-                last_message = value["messages"][-1]
-                if isinstance(last_message, dict) or last_message.type != "ai":
-                    continue
-                print(f"{node_id}: {last_message.content}")
-=======
 	thread_config: RunnableConfig = {
 		'configurable': {'thread_id': uuid.uuid4()},
 		'recursion_limit': 150,
@@ -71,4 +52,3 @@ for idx, user_input in enumerate(inputs):
 
 if __name__ == '__main__':
 	main()
->>>>>>> d28996abbab6940f4cf3f5935067d33af54f2be5
